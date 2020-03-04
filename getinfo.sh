1726a8093e9d1d24bb52bd4607f39a7b1ec1410d
tiagoceluppi <61787210+tiagoceluppi@users.noreply.github.com>
2020-03-04 10:27:14 -0300

    Update getinfo.sh

--- getinfo.sh
+++ getinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-# wget --no-cache --no-cookies  https://raw.githubusercontent.com/tiagoceluppi/repo/master/getinfo.sh;sh getinfo.sh ;rm -rf getinfo.sh 
+# wget --no-cache --no-cookies  https://raw.githubusercontent.com/tiagoceluppi/repo/master/getinfo.sh;sh getinfo.sh 
 hostname
 uptime
 df -h
